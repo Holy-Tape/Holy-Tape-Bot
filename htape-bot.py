e7a5d6f6859ae598dd6ff0008f50fc38ab04f8e3
Vladislav Aidarov <aidarovv@gmail.com>
2022-06-16 23:04:26 +0400

    add Request.data to logs

--- htape-bot.py
+++ htape-bot.py
@@ -115,6 +115,7 @@ def t_logger_event_handler():
 @app.route("/put-photo", methods=['GET', 'POST'])
 def photo_handler():
     if request.method == 'POST':
+        print('Request.data: ' + request.data)
         if 'image' not in request.files:
             print('No file part')
             return 'No file part', 400
